b823530296c6c5596acb6275aa2f939f5947bf73
Ketan <ketan.r1991@gmail.com>
2018-06-26 21:29:56 +0530

    Inserted new file into Git..

--- PDF_to_Word.py
+++ PDF_to_Word.py
@@ -4,8 +4,18 @@ from docx.shared import Inches
 
 pdfFileObj = open('C:/Users/krane/Desktop/New folder New/Ketan Ranebennur_Resume.pdf', 'rb') #path of your PDF file
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-print(pdfReader.numPages)
-pageObj = pdfReader.getPage(0)
-pageObj.extractText()
-f= open("C:/Users/krane/Desktop/New folder New/new_file.doc","w+") #Path of your DOC file to be saved
-f.write(pageObj.extractText()) #Write into the doc file
+k = pdfReader.numPages
+
+for i in range(k):
+    if(i==0):
+        pageObj = pdfReader.getPage(0)
+        pageObj.extractText()
+        f= open("C:/Users/krane/Desktop/New folder New/new_file.doc","w+") #Path of your DOC file to be saved
+        f.write(pageObj.extractText()) #Write into the doc file
+    else:
+        pageObj = pdfReader.getPage(i)
+        pageObj.extractText()
+        f = open("C:/Users/krane/Desktop/New folder New/new_file.doc", "a")  # Appends your existing Doc file
+        f.write(pageObj.extractText())  # Write into the doc file
+
+
